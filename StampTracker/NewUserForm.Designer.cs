7c73bb0a028c7091fd0448dc15904e88076c543b
Baidullayev Abylaikhan <abylai.baidu@gmail.com>
2016-11-16 20:03:29 +0600

    sdsd

--- StampTracker/NewUserForm.Designer.cs
+++ StampTracker/NewUserForm.Designer.cs
@@ -214,6 +214,7 @@
             // 
             // NewUserForm
             // 
+            this.AcceptButton = this.saveButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(306, 348);
